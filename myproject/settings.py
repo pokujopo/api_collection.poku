eb96de5e35443cf3f4273a4a099918f1ae45b068
pokujopo <pokujopo@gmail.com>
2026-06-26 12:21:40 +0300

    Change database configuration to use dj_database_url
    Updated database settings to use dj_database_url for configuration.

--- myproject/settings.py
+++ myproject/settings.py
@@ -71,11 +71,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'myproject.wsgi.application'
 
 # Database
+import dj_database_url
+import os
+
+# Kama upo Render, itasoma kamba ya Supabase. Kama ipo local na haujaweka, itatumia SQLite ya majaribio
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 # Password validation
